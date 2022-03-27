f9e4e496ea26cdf8aaa029502bf78cd5085bbc1b
Dina18013 <99754434+Dina18013@users.noreply.github.com>
2022-03-27 20:59:25 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double height) { // функция расчета индекса массы тела
 
-        double weight = 60;
-        double height = 1.7;
         double index = weight / (height * height); // расчет индекса массы тела
         return index;
     }
